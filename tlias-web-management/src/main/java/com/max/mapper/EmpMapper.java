8399039292c480be11a7055a0ca4030a9cadc171
Chen Kui <edrs8369@gmail.com>
2025-12-15 23:46:32 +0800

    員工管理-分頁查詢(改)-PageHelper

--- tlias-web-management/src/main/java/com/max/mapper/EmpMapper.java
+++ tlias-web-management/src/main/java/com/max/mapper/EmpMapper.java
@@ -11,13 +11,23 @@ import java.util.List;
 @Mapper
 public interface EmpMapper {
 
+
+
+    //---------------------------------------原始分頁查詢實現----------------------------------------------
+
     //分頁查詢
     //也需封裝部門名稱，所以需要在Emp添加deptName屬性
-    @Select("select emp.*, dept.name deptName from emp left join dept on emp.dept_id = dept.id " +
-            "order by emp.update_time desc limit #{start }, #{pageSize}")
-    List<Emp> list(Integer start, Integer pageSize);
+//    @Select("select emp.*, dept.name deptName from emp left join dept on emp.dept_id = dept.id " +
+//            "order by emp.update_time desc limit #{start }, #{pageSize}")
+//    List<Emp> list(Integer start, Integer pageSize);
 
     //查詢總紀錄數
-    @Select("select count(*) from emp e left join  dept d on e.dept_id = d.id")
-    Long count();
+//    @Select("select count(*) from emp e left join  dept d on e.dept_id = d.id")
+//    Long count();
+
+
+    // 普通的分頁查詢語句
+    @Select("select emp.*, dept.name deptName from emp left join dept on emp.dept_id = dept.id " +
+            "order by emp.update_time desc")
+    List<Emp> list();
 }
--- tlias-web-management/src/main/java/com/max/service/impl/EmpServiceImpl.java
+++ tlias-web-management/src/main/java/com/max/service/impl/EmpServiceImpl.java
@@ -1,5 +1,7 @@
 package com.max.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.max.mapper.EmpMapper;
 import com.max.pojo.Emp;
 import com.max.pojo.PageResult;
@@ -15,17 +17,36 @@ public class EmpServiceImpl implements EmpService {
     @Autowired
     private EmpMapper empMapper;
 
+    // 基於PageHelper實現分頁查詢
     @Override
     public PageResult<Emp> page(Integer page, Integer pageSize) {
 
-        //1.調用Mapper接口， 查詢總紀錄數
-        Long count = empMapper.count();
+        //1.設置分頁參數(PageHelper)
+        //startPage 設置分頁參數 → MyBatis 插件攔截下一條查詢 → 自動加 LIMIT/OFFSET → 結果封裝成 Page（實現 List）。
+        //因為多態的關係，Page的源碼繼承了 -> ArrayList ->　List
+        PageHelper.startPage(page, pageSize);
 
-        //2.調用Mapper接口， 查詢結果列表
-        Integer start = (page - 1) * pageSize;
-        List<Emp> rows = empMapper.list(start, pageSize);
+        //2.執行查詢
+        List<Emp> emplist = empMapper.list();
 
-        //3.封裝結果 PageResult
-        return new PageResult<Emp>(count, rows);
+        //3.解析查詢結果， 並封裝數據
+        //因為是多態，所以List0可以向下轉型
+        Page<Emp> p = (Page<Emp>) emplist;
+        return new PageResult<Emp>(p.getTotal(), p.getResult());
     }
+
+    //原始分頁查詢
+//    @Override
+//    public PageResult<Emp> page(Integer page, Integer pageSize) {
+//
+//        //1.調用Mapper接口， 查詢總紀錄數
+//        Long count = empMapper.count();
+//
+//        //2.調用Mapper接口， 查詢結果列表
+//        Integer start = (page - 1) * pageSize;
+//        List<Emp> rows = empMapper.list(start, pageSize);
+//
+//        //3.封裝結果 PageResult
+//        return new PageResult<Emp>(count, rows);
+//    }
 }
