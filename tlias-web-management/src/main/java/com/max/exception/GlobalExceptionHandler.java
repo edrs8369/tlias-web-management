d81d24b028e161b379a61eae497ab330d337998b
Chen Kui <edrs8369@gmail.com>
2025-12-18 18:52:39 +0800

    全局異常處理

---
+++ tlias-web-management/src/main/java/com/max/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.max.exception;
+
+import com.max.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+//全局異常處理器
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public Result handleException(Exception e){
+        log.error("程序出現錯誤~", e);
+        return Result.error("出錯啦, 請聯繫管理員");
+    }
+
+    @ExceptionHandler
+    public Result handleDuplicateKeyException(DuplicateKeyException e){
+        log.error("程序出現錯誤~", e);
+        String message = e.getMessage();
+        int index = message.indexOf("Duplicate entry");
+        String errMsg = message.substring(index);
+        String[] arr = errMsg.split(" ");
+        return Result.error(arr[2] + "已存在");
+    }
+}
